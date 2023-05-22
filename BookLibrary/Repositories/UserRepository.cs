360ca2c50dd8679a6cbdd8958b24e75d7a5a9704
MVMmaksM <vaganov.maksim94@yandex.ru>
2023-05-22 11:12:09 +0500

    Cretae GetById User

---
+++ BookLibrary/Repositories/UserRepository.cs
@@ -0,0 +1,24 @@
+﻿using BookLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLibrary.Repositories
+{
+    public class UserRepository
+    {
+        public User GetById(int Id)
+        {
+            User user;
+
+            using (var dbContext = new AppContext())
+            {
+                user = dbContext.Users.FirstOrDefault(u => u.Id.Equals(Id));
+            }
+
+            return user;
+        }
+    }
+}
