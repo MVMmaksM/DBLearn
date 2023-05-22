5d5e818816cad3c480c552034647e7818ae2fd3f
MVMmaksM <vaganov.maksim94@yandex.ru>
2023-05-22 13:45:55 +0500

    Create classs Author

--- BookLibrary/Models/Book.cs
+++ BookLibrary/Models/Book.cs
@@ -11,5 +11,11 @@ namespace BookLibrary.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public int Year { get; set; }
+        public int UserId { get; set; }
+        public int authorId { get; set; }
+        public int genreId { get; set; }
+        public User User { get; set; }
+        public Author Author { get; set; }
+        public Genre Genre { get; set; }
     }
 }
--- BookLibrary/Models/User.cs
+++ BookLibrary/Models/User.cs
@@ -11,5 +11,6 @@ namespace BookLibrary.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+        public List<Book> Books { get; set; } = new List<Book>();
     }
 }
